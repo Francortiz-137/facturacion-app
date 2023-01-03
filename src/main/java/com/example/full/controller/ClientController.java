aff0c94cd0d5a79a82a169cd23fd9feed98e783c
Francortiz-137 <francortiz.c@gmail.com>
2023-01-03 13:19:31 -0300

    fixed controller and service added CORS

--- src/main/java/com/example/full/controller/ClientController.java
+++ src/main/java/com/example/full/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.example.full.controller;
 
 import com.example.full.model.Client;
 import com.example.full.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RequestMapping("/api/clientes")
 public class ClientController {
 
+    @Autowired
     private static ClientService clientService;
 
-    @GetMapping("/")
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping("")
     public List<Client> listClients(){
         return  clientService.findAll();
     }
--- src/main/java/com/example/full/service/impl/ClientServiceImpl.java
+++ src/main/java/com/example/full/service/impl/ClientServiceImpl.java
@@ -3,16 +3,18 @@ package com.example.full.service.impl;
 import com.example.full.model.Client;
 import com.example.full.repository.ClientRepository;
 import com.example.full.service.ClientService;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@AllArgsConstructor
 public class ClientServiceImpl implements ClientService {
 
     private static ClientRepository clientRepository;
+
+    public ClientServiceImpl(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
 
     @Override
     public List<Client> findAll() {
