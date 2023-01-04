250f3b6c5aeb05e36597886e873e77625e4cdf9a
Francortiz-137 <francortiz.c@gmail.com>
2023-01-04 12:43:21 -0300

    Added crud endpoints in ClientController

--- src/main/java/com/example/full/controller/ClientController.java
+++ src/main/java/com/example/full/controller/ClientController.java
@@ -3,11 +3,11 @@ package com.example.full.controller;
 import com.example.full.model.Client;
 import com.example.full.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Timestamp;
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -27,4 +27,29 @@ public class ClientController {
         return  clientService.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Client readClient(@PathVariable Long id){
+        return clientService.findById(id);
+    }
+
+    @PostMapping("")
+    public Client create(@RequestBody Client client){
+        client.setCreatedAt(Date.from(Instant.now()));
+        return clientService.save(client);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        clientService.delete(id);
+    }
+
+    @PutMapping("{id}")
+    public Client update(@RequestBody Client client, @PathVariable Long id){
+        Client currentClient = clientService.findById(id);
+        currentClient.setName(client.getName());
+        currentClient.setEmail(client.getEmail());
+        currentClient.setLastName(client.getLastName());
+
+        return clientService.save(currentClient);
+    }
 }
