f0d035182049d49ce9c57a0aa7d71d75302bbb84
Francortiz-137 <francortiz.c@gmail.com>
2023-01-26 15:34:28 -0300

    Added AuthorizationServerConfig (Deprecated)

---
+++ src/main/java/com/example/full/auth/SpringSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.full.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private UserDetailsService userService;
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Autowired
+    private void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+}
