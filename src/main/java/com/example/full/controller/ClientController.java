c4069e0b3b7f0cb04419e7f697725e9bc2414066
Francortiz-137 <francortiz.c@gmail.com>
2023-02-05 21:53:55 -0300

    changes to deploy in production

--- src/main/java/com/example/full/controller/ClientController.java
+++ src/main/java/com/example/full/controller/ClientController.java
@@ -27,7 +27,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins= {"http://localhost:4200"})
+@CrossOrigin(origins= {"http://localhost:4200","*"})
 @RequestMapping("/api/clientes")
 public class ClientController {
 
--- src/main/java/com/example/full/controller/InvoiceController.java
+++ src/main/java/com/example/full/controller/InvoiceController.java
@@ -9,7 +9,7 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins= {"http://localhost:4200"})
+@CrossOrigin(origins= {"http://localhost:4200","*"})
 @RequestMapping("/api")
 public class InvoiceController {
 
